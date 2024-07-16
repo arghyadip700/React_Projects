69f36d3d8a8067bc9165b4b77f80ac89e5561957
Arghyadip Dhara <arghyadipdhara1@gmail.com>
2024-07-16 11:47:47 +0530

    Counter Project created

---
+++ 01counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  let [counter, setCounter] = useState(5)
+
+  // let counter = 5
+
+  const addValue = () => {
+    // counter = counter + 1
+    if(counter<20){
+      setCounter(counter + 1)
+    }
+  }
+
+  const decreaseValue = () => {
+    if(counter>0){
+      setCounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1>Learning React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button>
+      <br />
+      <button
+      onClick={decreaseValue}
+      >Decrease value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
